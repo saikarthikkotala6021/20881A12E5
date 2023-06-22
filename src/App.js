fdaa412a644e6b5b988ce6e2de34a96c2e8f2f2b
KOTALA SAI KARTHIK <93485351+saikarthikkotala6021@users.noreply.github.com>
2023-06-22 12:13:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import NumberManagement from './NumberManagement';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Number Management App</h1>
+      <NumberManagement />
+    </div>
+  );
+}
+
+export default App;
+
